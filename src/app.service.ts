a0c9e8924b5ed26b79eea9057b4130a02f588b79
dorinpuscasu <dorin@puscasu>
2023-07-04 16:47:18 +0300

    redis test connection

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@nestjs/common';
+import Redis from 'ioredis';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  private redisClient: Redis
+
+  constructor(){
+    this.redisClient = new Redis({
+      host: '127.0.0.1',
+      port: 6379,
+    });
+  
+  }
+
+  async testConnection(): Promise<void> {
+
+    await this.redisClient.set('myname', 'Dorin Puscasu');
+
+    // Get the value held at key "myname" and log it.
+    const value = await this.redisClient.get('myname');
+    console.log(value);
+  
   }
 }
